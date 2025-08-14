2e3b6072bb574b6ae58aee2ea95c6992cc9e6034
Ali Egecan Özener <94221528+AliEgecanOzener@users.noreply.github.com>
2025-08-14 15:01:30 +0300

    Update dnsrecon.py

--- dnsrecon.py
+++ dnsrecon.py
@@ -12,15 +12,10 @@ class DNSRecon:
         self.utils = Utils(self.url)
         self.cookie = self.utils.convert_string_to_dict(cookie)
         self.proxy = proxy
+
         self.basic_auth_creds = basic_auth_creds
-
         self.user_agent = self.utils.get_random_user_agent()
-
-        if self.basic_auth_creds:
-            self.headers = {"User-Agent": self.user_agent}
-
-        else:
-            self.headers = {"User-Agent": self.user_agent}
+        self.headers = {"User-Agent": self.user_agent}
 
         self.records = {
             "A": [],
@@ -36,13 +31,6 @@ class DNSRecon:
         self.zone = []
         self.whois = None
 
-    def is_valid_target(self):
-        if self.utils.is_valid_url(self.url):
-            domain = self.utils.get_base_domain(self.url)
-            return self.utils.is_valid_domain(domain)
-        elif self.utils.is_valid_domain(self.url):
-            return True
-        return False
 
     def append_list(self, list_obj, data):
         for d in data:
@@ -52,7 +40,7 @@ class DNSRecon:
 
 
     def check_record(self, record_type):
-        if not self.is_valid_target():
+        if not self.utils.is_valid_target():
             print(colored("[WARNING] Invalid target. Quitting...", "light_red"))
             return None
 
@@ -61,7 +49,7 @@ class DNSRecon:
                 if not self.records["A"]:
                     self.check_record("A")
                 if not self.records["A"]:
-                    print(colored("[INFO] No A record found. Cannot do PTR lookup.", "yellow"))
+                    #print(colored("[INFO] No A record found. Cannot do PTR lookup.", "yellow"))
                     return None
                 ip = self.records["A"][0]
                 rev_name = dns.reversename.from_address(ip)
@@ -76,61 +64,74 @@ class DNSRecon:
         except dns.resolver.NoAnswer:
             pass
         except dns.resolver.NXDOMAIN:
-            print(colored(f"[ERROR] Domain {self.utils.get_base_domain(self.url)} does not exist", "red"))
+            #print(colored(f"[ERROR] Domain {self.utils.get_base_domain(self.url)} does not exist", "red"))
+            pass
         except dns.resolver.LifetimeTimeout:
-            print(
-                colored(f"[ERROR] Timeout while querying {record_type} records for {self.utils.get_base_domain(self.url)}", "red"))
+            #print(colored(f"[ERROR] Timeout while querying {record_type} records for {self.utils.get_base_domain(self.url)}", "red"))
+            pass
         except Exception as e:
-            print(colored(f"[ERROR] {record_type} lookup failed: {e}", "red"))
+            #print(colored(f"[ERROR] {record_type} lookup failed: {e}", "red"))
+            pass
 
         return None
 
     def axfr(self):
-        print(colored("************************************************************************************************","yellow", attrs=['bold']))
-        print(colored("AXFR Check", "yellow", attrs=['bold']))
-        print(colored("************************************************************************************************","yellow", attrs=['bold']))
-        if not self.is_valid_target():
+        #print(colored("*" * 100, "yellow", attrs=['bold']))
+        #print(colored("AXFR Check", "yellow", attrs=['bold']))
+        #print(colored("*" * 100, "yellow", attrs=['bold']))
+
+        if not self.utils.is_valid_target():
             print("[INFO] Given target is invalid. Quitting...")
-            return None
+            return []
 
         domain = self.utils.get_base_domain(self.url)
 
         if not self.records['NS']:
             nameservers = self.check_record("NS")
             if not nameservers:
-                print(f"[INFO] No NS record found for {self.url}. Quitting...")
-                return None
+                #print(f"[INFO] No NS record found for {self.url}. Quitting...")
+                return []
 
         for server in self.records['NS']:
-
             try:
                 server_domain = server.strip(".")
                 server_ip = self.utils.get_ip_info(server_domain)
-                print(f"[INFO] Trying AXFR from {server_domain} ({server_ip}) for {domain}")
+                #print(f"[INFO] Trying AXFR from {server_domain} ({server_ip}) for {domain}")
 
                 zone = dns.zone.from_xfr(dns.query.xfr(server_ip, domain))
-                print(colored(f"[SUCCESS] Zone transfer successful from {server_domain}", "yellow"))
+                #print(colored(f"[SUCCESS] Zone transfer successful from {server_domain}", "yellow"))
                 self.zone.append((server_domain, zone))
 
             except Exception as e:
-                print(f"[ERROR] Zone transfer failed for {server}: {e}")
+                #print(f"[ERROR] Zone transfer failed for {server}: {e}")
                 continue
+
+        return self.zone
+
+
+
     def return_axfr_subdomains(self):
         subdomains = set()
-        if not self.zone:
-            self.zone = self.axfr()
 
         if not self.zone:
-           return
+            self.axfr()
+
+        if not self.zone:
+            return subdomains
 
         domain = self.utils.get_base_domain(self.url)
-        for name, node in self.zone.items():
-            fqdn = f"{name}.{domain}" if str(name) != "@" else domain
-            subdomains.add(fqdn)
+
+        for server_domain, zone in self.zone:
+            for name, node in zone.nodes.items():
+                fqdn = f"{name}.{domain}" if str(name) != "@" else domain
+                if "IN-ADDR.ARPA".lower() not in fqdn.lower():
+                    subdomains.add(fqdn)
+
         return subdomains
 
+
     def whois_lookup(self):
-        if not self.is_valid_target():
+        if not self.utils.is_valid_target():
             print(colored("[WARNING] Invalid target. Quitting...", "light_red"))
             return None
         domain = self.utils.get_base_domain(self.url)
@@ -141,11 +142,11 @@ class DNSRecon:
             return self.whois
 
         except Exception as e:
-            print(f"[ERROR] {e}")
+            #print(f"[ERROR] {e}")
+            pass
 
 
     def print_axfr(self):
-
         if not self.zone:
             print("[INFO] No zone transfer data available.")
             return
@@ -165,11 +166,10 @@ class DNSRecon:
         print(colored("************************************************************************************************","yellow", attrs=['bold']))
         print(colored("[INFO] Whois records...", "yellow", attrs=['bold']))
         print(colored("************************************************************************************************","yellow", attrs=['bold']))
-
         print(self.whois)
 
-    def print_dns_records(self, record_type):
 
+    def print_dns_records(self, record_type):
         records = self.records.get(record_type, [])
         if not records:
             print(colored(f"[INFO] No {record_type} records found for {self.utils.get_base_domain(self.url)}", "yellow"))
@@ -177,27 +177,3 @@ class DNSRecon:
         print(colored(f"[INFO] {record_type} records:", "yellow"))
         for data in records:
             print(data)
-
-"""
-url = "microsoft.com"
-hit_dns = DNSRecon(url, "", "", "")
-
-hit_dns.whois_lookup()
-hit_dns.print_whois()
-
-hit_dns.axfr()
-hit_dns.print_axfr()
-
-print(colored("************************************************************************************************",
-              "yellow", attrs=['bold']))
-print(colored("[INFO] DNS records...", "yellow", attrs=['bold']))
-print(colored("************************************************************************************************",
-              "yellow", attrs=['bold']))
-
-for rtype in ["A", "AAAA", "NS", "MX", "TXT", "CNAME", "SOA", "PTR"]:
-    hit_dns.check_record(rtype)
-    hit_dns.print_dns_records(rtype)
-"""
-
-
-
